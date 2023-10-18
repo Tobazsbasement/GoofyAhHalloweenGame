2b39c441de6f1bb8d0c429c6684cfed29f67d9f5
Tobazsbasement <friendlypieceofshit@gmail.com>
2023-10-18 10:36:31 +0200

    State Machine

--- Scripts/Character Components/Shared/VelocityComponent.cs
+++ Scripts/Character Components/Shared/VelocityComponent.cs
@@ -9,6 +9,10 @@ public partial class VelocityComponent : Node
 	public int Acceleration;
 	[Export]
 	public int Friction;
+	[Export]
+	public int SprintMax;
+	[Export]
+	public int SprintAcceleration;
 
 	public Vector3 Accelerate(Vector3 Velocity, double delta){
 		Velocity.X *= Acceleration * (float)delta;
@@ -20,9 +24,19 @@ public partial class VelocityComponent : Node
 		return Velocity;
 	}
 
+	public Vector3 Sprint(Vector3 Velocity, double delta){
+		Velocity.X *= SprintAcceleration * (float)delta;
+		Velocity.Z *= SprintAcceleration * (float)delta;
+
+		Velocity.X = Mathf.Clamp(Velocity.X, -SprintMax, SprintMax);
+		Velocity.Z = Mathf.Clamp(Velocity.Z, -SprintMax, SprintMax);
+
+		return Velocity;
+
+	}
 	public Vector3 ApplyFriction(Vector3 Velocity){
-		Velocity.X = Mathf.Lerp(Velocity.X, 0, Friction);
-		Velocity.Z = Mathf.Lerp(Velocity.Z, 0, Friction);
+		Velocity.X = Mathf.MoveToward(Velocity.X, 0, Friction);
+		Velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Friction);
 
 		return Velocity;
 	}
--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -6,16 +6,28 @@ public partial class Player : CharacterBody3D
 	private Vector3 InputVector;
 	public Vector3 CalculatedVelocity;
 	private PlayerCamera Camera;
+	private AnimationPlayer AnimPlayer;
+	private bool IsSprinting;
+	public enum States {
+		IDLE,
+		MOVING
+	}
+	public States CurrentState;
+
 	[Export]
 	private VelocityComponent VelocityNode;
 
     public override void _Ready()
     {
         Camera = GetNode<PlayerCamera>("PlayerCamera");
+		AnimPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
     }
+
     private Vector3 HandleInput(double delta){
 		InputVector.X = Input.GetActionStrength("Right") - Input.GetActionStrength("Left");
 		InputVector.Z = Input.GetActionStrength("Backwards") - Input.GetActionStrength("Forwards");
+
+		IsSprinting = Input.IsActionPressed("Sprint");
 
 		return InputVector;
 	}
@@ -24,13 +36,44 @@ public partial class Player : CharacterBody3D
     {
 		HandleInput(delta);
 
-		if(InputVector == Vector3.Zero){
-			Velocity = Vector3.Zero;
-		}
-		else{
-			Velocity = VelocityNode.Accelerate(InputVector, delta).Normalized() * Camera.Transform.Basis;
-		}
+		StateMachine(delta);
 
 		MoveAndSlide();
     }
+
+	private void ApplyMovement(double delta){
+		if(IsSprinting){
+			Velocity = VelocityNode.Sprint(InputVector, delta).Normalized() * Camera.Transform.Basis;
+
+			AnimPlayer.Play("Run");
+		}
+		else{
+			Velocity = VelocityNode.Accelerate(InputVector, delta).Normalized() * Camera.Transform.Basis;
+			
+			AnimPlayer.Play("Walk");
+		}
+	}
+
+	private void StateMachine(double delta){
+		if(InputVector == Vector3.Zero){
+			CurrentState = States.IDLE;
+		}
+		else if(InputVector != Vector3.Zero){
+			CurrentState = States.MOVING;
+		}
+
+		switch(CurrentState){
+			case States.IDLE:
+				Velocity = VelocityNode.ApplyFriction(Velocity);
+
+				AnimPlayer.Play("Idle");
+
+				break;
+			
+			case States.MOVING:
+				ApplyMovement(delta);
+
+				break;
+		}
+	}
 }
